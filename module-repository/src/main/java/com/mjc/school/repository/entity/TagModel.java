71b2eddf2e173908542b37bdf9ab25b44ca2f706
Damian Cegielka <cegielka.damian@gmail.com>
2023-06-27 15:19:02 +0200

    Add TagModel class in repository module

---
+++ module-repository/src/main/java/com/mjc/school/repository/entity/TagModel.java
@@ -0,0 +1,15 @@
+package com.mjc.school.repository.entity;
+
+import com.mjc.school.repository.model.BaseEntity;
+
+public class TagModel  implements BaseEntity<Long> {
+    @Override
+    public Long getId() {
+        return null;
+    }
+
+    @Override
+    public void setId(Long id) {
+
+    }
+}
