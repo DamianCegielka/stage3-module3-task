63fde377853c13b91664e1c6a561e27034c922b1
Damian Cegielka <cegielka.damian@gmail.com>
2023-06-28 18:17:19 +0200

    Add properties for DB and annotations for main class

--- module-main/src/main/java/com/mjc/school/main/Main.java
+++ module-main/src/main/java/com/mjc/school/main/Main.java
@@ -1,14 +1,19 @@
 package com.mjc.school.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.io.IOException;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @EnableJpaAuditing
+@ComponentScan("com.mjc.school.*")
 public class Main {
     public static void main(String[] args) throws IOException {
         ApplicationContext context = SpringApplication.run(Main.class, args);
