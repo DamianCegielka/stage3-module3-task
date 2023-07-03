0447055242bd4f159567d4ccb2ce4493679ed48e
Damian Cegielka <cegielka.damian@gmail.com>
2023-07-03 15:33:00 +0200

    add TagService implementation

--- module-service/src/main/java/com/mjc/school/service/impl/TagService.java
+++ module-service/src/main/java/com/mjc/school/service/impl/TagService.java
@@ -1,4 +1,36 @@
 package com.mjc.school.service.impl;
 
-public class TagService {
+import com.mjc.school.service.BaseService;
+import com.mjc.school.service.dto.tag.TagDtoRequest;
+import com.mjc.school.service.dto.tag.TagDtoResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TagService implements BaseService<TagDtoRequest, TagDtoResponse, Long> {
+    @Override
+    public List<TagDtoResponse> readAll() {
+        return null;
+    }
+
+    @Override
+    public TagDtoResponse readById(Long id) {
+        return null;
+    }
+
+    @Override
+    public TagDtoResponse create(TagDtoRequest createRequest) {
+        return null;
+    }
+
+    @Override
+    public TagDtoResponse update(TagDtoRequest updateRequest) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        return false;
+    }
 }
